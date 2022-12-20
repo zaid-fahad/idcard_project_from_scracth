0ca15fa368dddf8a8abf29228c88a5e3c98f44ca
Zaid Fahad <fahadbinrazib@gmail.com>
2022-12-20 23:52:18 +0600

    layou updated , std image aded

--- studentSingle.php
+++ studentSingle.php
@@ -17,7 +17,11 @@ $result = $conn->query($sql);
 $conn = null;
 
 ?>
-<div class="container card m-1">
+<div style="
+    display: grid !important;
+    justify-content: space-around;
+    align-items: end;
+    justify-items: center;">
 
     <?php foreach ($result as $row) {
         // $sid = $row['S_id'];
@@ -33,41 +37,42 @@ $conn = null;
         $address = $row['Address'];
         $photo = $row['Photo'];
     ?>
+<div class="card m-4">
+<p class="card-header h4 text-center text-bg-secondary"> Student Information </p>
+ <!-- <div class="h3 p-3"> <center>Student Information</center></div>    -->
+<img src="<?php echo 'http://127.0.0.1:2525/idcard_project'  . '/image/std/' . $photo; ?>" class="img-thumbnail rounded" width="200px" style="align-self:center; margin: 30px 0px 15px 0px;" alt="srd pic">
 
-        <div class="row m-2">
-            <div class="col-4 "><img src="<?php echo 'http://127.0.0.1:2525/idcard_project'  . '/image/std/' . $photo; ?>" class="img-thumbnail rounded float-left m-2" width="200px" alt="srd pic"></div>
-            <div class="card-body col-8 p-3">
-                <div>
-                    <h5 class="card-title">Name : <?php echo  $name; ?></h5>
-                    <p class="card-text">Address: <?php echo $address; ?>.</p>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">SID: <?php echo $sid; ?></li>
-                        <li class="list-group-item">Phone: <?php echo $phone; ?> </li>
-                        <li class="list-group-item">
-                            <div class="d-flex ">
-                                <ul class="list-group m-2 p-1">
-                                    <dt> Father: <?php echo $Fname; ?></dt>
-                                    <dd> Work: <?php echo $Fwork; ?>
-                                        </br>Phone: <?php echo $Fphone; ?></dd>
-                                </ul>
-                                <ul class="list-group m-2 p-1">
-                                    <dt> Mother: <?php echo $Mname; ?></dt>
-                                    <dd> Work: <?php echo $Mwork; ?>
-                                        </br>Phone: <?php echo $Mphone; ?></dd>
-                                </ul>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-                <div>
-                    <a class="btn btn-secondary" href="/idcard_project/index.php/<?php echo $sid; ?>" role="button">Back</a></td>
-                    <a class="btn btn-secondary" href="/idcard_project/addStudent.php/<?php echo $sid; ?>" role="button">Edit</a></td>
-                </div>
-            </div>
-        <?php } ?>
-
+        <div class="card-body">
+            <h5 class="card-title">Name : <?php echo  $name; ?></h5>
         </div>
-        <!-- </table> -->
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">SID: <?php echo $sid; ?></li>
+            <li class="list-group-item">Phone: <?php echo $phone; ?> </li>
+            <li class="list-group-item">Address: <?php echo $address; ?>.</li>
+            
+            <li class="list-group-item">
+            <div class="d-flex justify-content-between ">
+                <ul class="list-group m-2">
+                    <dt> Father: <?php echo $Fname; ?></dt>
+                    <dd> Work: <?php echo $Fwork; ?>
+                    </br>Phone: <?php echo $Fphone; ?></dd>
+                </ul>
+                <ul class="list-group m-2">
+                    <dt> Mother: <?php echo $Mname; ?></dt>
+                    <dd> Work: <?php echo $Mwork; ?>
+                    </br>Phone: <?php echo $Mphone; ?></dd>
+                </ul>
+            </div>
+            </li>
+        </ul>
+        <div class="card-body d-flex justify-content-center">
+            <a class="btn btn-secondary m-1" href="/idcard_project/index.php/<?php echo $sid; ?>" role="button">Back</a></td>
+            <a class="btn btn-secondary m-1" href="/idcard_project/addStudent.php/<?php echo $sid; ?>" role="button">Edit</a></td>
+        </div>
+    <?php } ?>
+
+</div>
+    <!-- </table> -->
 
 </div>
 
